e3e9742957178574804f0d401a1c579587bb5e24
Rachel Kong <rachel.kong95@gmail.com>
2021-12-07 16:29:27 +0800

    set up class

---
+++ BattleshipGame.java
@@ -0,0 +1,17 @@
+
+public class BattleshipGame {
+
+	// to include instance variable type Ocean
+
+	public void start() {
+		// TODO: to implement main method
+
+		// accept shots from user as coordinates
+
+		// display results
+
+		// print final scores
+
+		// ask if user wants to play again
+	}
+}
