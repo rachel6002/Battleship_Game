59a57126f8afb84bf05e2d43684111371ba53119
Rachel Kong <rachel.kong95@gmail.com>
2021-12-11 19:59:46 +0800

    Updated constructor

--- Destroyer.java
+++ Destroyer.java
@@ -1,23 +1,23 @@
 /**
  * A ship with a length of two tiles.
  *
- * @author Huifang Ye
+ * @author Huifang Ye & Rachel Kong
  */
 public class Destroyer extends Ship {
-    /**
-     * Sets the inherited length variable and initializes the
-     * hit array, based on the size of this ship (2 tiles).
-     */
-    public Destroyer() {
+	/**
+	 * Sets the inherited length variable and initializes the hit array, based on
+	 * the size of this ship (2 tiles).
+	 */
+	public Destroyer() {
+		length = 2;
+	}
 
-    }
-
-    /**
-     *
-     * @return "Destroyer", indicating that this is a Destroyer.
-     */
-    @Override
-    public String getShipType() {
-        return "Destroyer";
-    }
+	/**
+	 *
+	 * @return "Destroyer", indicating that this is a Destroyer.
+	 */
+	@Override
+	public String getShipType() {
+		return "Destroyer";
+	}
 }
