689a044788a5ee66e5864624e8a501873bbf1531
Rachel Kong <rachel.kong95@gmail.com>
2021-12-11 19:57:33 +0800

    Updated constructor

--- Battleship.java
+++ Battleship.java
@@ -1,23 +1,23 @@
 /**
  * A Ship with a length of four tiles.
  *
- * @author Huifang Ye
+ * @author Huifang Ye & Rachel Kong
  */
-public class Battleship extends Ship{
-    /**
-     * Sets the inherited length variable and initializes the
-     * hit array, based on the size of this ship (4 tiles).
-     */
-    public Battleship() {
+public class Battleship extends Ship {
+	/**
+	 * Sets the inherited length variable and initializes the hit array, based on
+	 * the size of this ship (4 tiles).
+	 */
+	public Battleship() {
+		length = 4;
+	}
 
-    }
-
-    /**
-     *
-     * @return "Battleship", indicating that this is a Battleship.
-     */
-    @Override
-    public String getShipType() {
-        return "Battleship";
-    }
+	/**
+	 *
+	 * @return "Battleship", indicating that this is a Battleship.
+	 */
+	@Override
+	public String getShipType() {
+		return "Battleship";
+	}
 }
