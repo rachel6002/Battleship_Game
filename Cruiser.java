6b3445f648b64725914858be753a613c0029fb3b
Rachel Kong <rachel.kong95@gmail.com>
2021-12-11 19:58:25 +0800

    Updated constructor

--- Cruiser.java
+++ Cruiser.java
@@ -1,23 +1,23 @@
 /**
  * A ship with a length of three tiles.
  *
- * @author Huifang Ye
+ * @author Huifang Ye & Rachel Kong
  */
 public class Cruiser extends Ship {
-    /**
-     * Sets the inherited length variable and initializes the hit
-     * array, based on the size of this ship (3 tiles).
-     */
-    public Cruiser() {
+	/**
+	 * Sets the inherited length variable and initializes the hit array, based on
+	 * the size of this ship (3 tiles).
+	 */
+	public Cruiser() {
+		length = 3;
+	}
 
-    }
-
-    /**
-     *
-     * @return "Cruiser", indicating that this is a Cruiser.
-     */
-    @Override
-    public String getShipType() {
-        return "Cruiser";
-    }
+	/**
+	 *
+	 * @return "Cruiser", indicating that this is a Cruiser.
+	 */
+	@Override
+	public String getShipType() {
+		return "Cruiser";
+	}
 }
