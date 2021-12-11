8d32c5905a9de85db0b8f978725f4401231ec47e
Rachel Kong <rachel.kong95@gmail.com>
2021-12-11 19:59:06 +0800

    Updated constructor

--- Submarine.java
+++ Submarine.java
@@ -1,24 +1,23 @@
 /**
  * A ship with a length of one tile.
  *
- * @author Huifang Ye
+ * @author Huifang Ye & Rachel Kong
  */
 public class Submarine extends Ship {
-    /**
-     * Sets the inherited length variable and
-     * initializes the hit array, based on the
-     * size of this ship (1 tiles).
-     */
-    public Submarine() {
+	/**
+	 * Sets the inherited length variable and initializes the hit array, based on
+	 * the size of this ship (1 tiles).
+	 */
+	public Submarine() {
+		length = 1;
+	}
 
-    }
-
-    /**
-     *
-     * @return "Submarine", indicating that this is a Submarine.
-     */
-    @Override
-    public String getShipType() {
-        return "Submarine";
-    }
+	/**
+	 *
+	 * @return "Submarine", indicating that this is a Submarine.
+	 */
+	@Override
+	public String getShipType() {
+		return "Submarine";
+	}
 }
